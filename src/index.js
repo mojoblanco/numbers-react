0c99613efad04f06df11fdfe39a38c7b2b2334ef
John Olawale <mojoblanco@gmail.com>
2019-05-26 18:17:22 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "bootswatch/dist/yeti/bootstrap.css";
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
